7ca623a4bf077f5fa080068614bdf91eee8538a7
Maaax02 <massiown@gmail.com>
2022-03-13 21:37:15 +0100

    task completed

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><ExampleComponentVue></ExampleComponentVue>
+  </div>
+</template>  
+<script>
+import ExampleComponent from '../../../vendor/laravel/ui/src/Presets/vue-stubs/ExampleComponent.vue';
+import ExampleComponentVue from "../components/ExampleComponent.vue";
+export default 
+{ components: 
+    { ExampleComponentVue, ExampleComponent } 
+};
+</script>  
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')
+->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/posts', 'PostController@index')->name('post.index');
+    Route::get('/posts/create', 'PostController@index')->name('post.create');
+    Route::get('/posts/edit', 'PostController@index')->name('post.edit');
 });
+
+Route::get('{any?}', function(){
+    return view('home');
+})->where('any', '.*');
